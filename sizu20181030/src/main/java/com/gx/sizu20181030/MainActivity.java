9f42dbf252e691f3ac4f2e65e057d36a59e168b1
金子射爱情、 <1569973744@qq.com>
2018-10-30 15:46:34 +0800

    与规划规划

---
+++ sizu20181030/src/main/java/com/gx/sizu20181030/MainActivity.java
@@ -0,0 +1,15 @@
+package com.gx.sizu20181030;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //gaoxin分支
+
+    }
+}
